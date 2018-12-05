f2f418aaa1077178a8f7472863fa773733664888
LukasMendez <43059840+LukasMendez@users.noreply.github.com>
2018-12-05 12:04:04 +0100

    Starting point

---
+++ src/sample/Square.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class Square {
+
+    private Canvas canvasBackground;
+
+    private int positionX;
+    private int getPositionY;
+    private int size;
+    private Color color;
+
+
+
+
+
+    public Square(int positionX, int positionY, int size, Color color){
+
+        this.positionX=positionX;
+        this.getPositionY=positionY;
+        this.size=size;
+        this.color=color;
+
+
+    }
+
+
+    public void drawSquare(Square name){
+
+        for (int i = 1; i <= canvasBackground.getWidth(); i=i+75) {
+
+            System.out.println("shit");
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
