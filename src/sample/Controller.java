5e49e705e4031fddad30aa727d76a78aed0f7d90
LukasMendez <43059840+LukasMendez@users.noreply.github.com>
2018-12-05 12:18:08 +0100

    Starting point

---
+++ src/sample/Controller.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+
+public class Controller {
+
+
+
+    @FXML
+    public void initialize(){
+
+        //TODO METHODS THAT WILL RUN WHEN APPLICATION IS LAUNCHED
+
+        Square sc = new Square(1,1,75, Color.BLACK);
+
+        sc.drawSquare(sc);
+
+    }
+
+
+
+
+
+
+
+
+
+}
